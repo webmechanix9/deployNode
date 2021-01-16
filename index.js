6c081b06afe238a6b217fda30a407319a8571733
Shubham Verma <shubham.verma@rws.com>
2021-01-17 00:07:49 +0530

    FE

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const HOST = "0.0.0.0";
 const PORT = 3000;
-
-app.get('/', (req, res) => {
-    res.send("Hi Shubham...!");
+app.use(express.static('./dist/login'));
+app.get('/login', (req, res) => {
+    res.sendFile(require('./dist/'));
 });
 
 app.listen(PORT,HOST,()=>console.log(`Server is Hosted on ${HOST} and using port ${PORT}`));
